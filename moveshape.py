74bc16ae8f3198a47d63a28518af46a211df1db2
kimhj3715 <benjamin3726@hotmail.com>
2015-10-22 17:27:02 -0700

    remove

--- moveshape.py
+++ moveshape.py
@@ -3,6 +3,7 @@ import moviepy
 import gizeh
 import numpy as np
 
+
 clip = VideoClip(make_frame, duration=4) # for custom animations (see below)
 clip = VideoFileClip("my_video_file.mp4") # or .avi, .webm, .gif ...
 clip = ImageSequenceClip(['moving1.png', 'moving2.png', 'moving3.png', 'moving4.png', 'moving5.png', ], fps=24)
